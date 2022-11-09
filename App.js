6ff9c4503affbd224e9ae5c8089e9d1a218d030c
Asdrúbal de Elejalde <asdrubalolocco@gmail.com>
2022-11-09 18:41:44 -0300

    correciones

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Header from './components/Header';
+import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Header title = {"Adivina el número"}/>
+      <StartGameScreen/>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
---
+++ components/Header.js
@@ -0,0 +1,28 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import Colors from '../constants/Colors'
+
+const Header = ({title}) => {
+    return (
+        <View style = {styles.header}>
+            <Text style = {styles.headerTitle}>{title}</Text>
+        </View>
+    )
+}
+
+export default Header
+
+const styles = StyleSheet.create({
+    header: {
+        backgroundColor: Colors.primary,
+        height: 90,
+        paddingTop: 36,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    headerTitle: {
+        color: Colors.white,
+        fontSize: 22,
+    }
+    
+})
